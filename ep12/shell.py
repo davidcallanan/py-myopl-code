6522535b2308ecda09b35681cfefe47adccc981a
davidcallanan <40340425+davidcallanan@users.noreply.github.com>
2019-07-10 09:45:22 +0100

    add: ep12

---
+++ ep12/shell.py
@@ -0,0 +1,13 @@
+import basic
+
+while True:
+	text = input('basic > ')
+	result, error = basic.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
