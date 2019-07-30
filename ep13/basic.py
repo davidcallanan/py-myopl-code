26476e2f760c645ad861b1ab686f34c703230b29
davidcallanan <40340425+davidcallanan@users.noreply.github.com>
2019-07-30 14:36:24 +0100

    fix if condition was statement instead of expr

--- ep13/basic.py
+++ ep13/basic.py
@@ -936,7 +936,7 @@ class Parser:
     res.register_advancement()
     self.advance()
 
-    condition = res.register(self.statement())
+    condition = res.register(self.expr())
     if res.error: return res
 
     if not self.current_tok.matches(TT_KEYWORD, 'THEN'):
